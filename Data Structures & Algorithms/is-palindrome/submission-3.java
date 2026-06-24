60f8f9596b4c614e924e0be2ce956bdb02ca71b7
Josue Garcia <49653057+josue-garcia777@users.noreply.github.com>
2026-06-24 06:58:41 -0600

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String sanitized = s.replaceAll("[,:?'.\\s]", "").trim().toLowerCase();
+        int n = sanitized.length();
+
+        int l =0;
+        int r = n-1;
+        
+        while(l <= r){
+            if(sanitized.charAt(l) != sanitized.charAt(r)){
+                return false;
+            }
+            
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+}
