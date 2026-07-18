a5db5d72175b8cafeb8fc2d637b9a3c61e44ad46
Josue Garcia <49653057+josue-garcia777@users.noreply.github.com>
2026-07-18 09:22:22 -0600

    Add: largest-number - submission-2

---
+++ Data Structures & Algorithms/largest-number/submission-2.java
@@ -0,0 +1,56 @@
+class Solution {
+    /*
+    The idea actually is treat numbers as String then use a comparator betwen curr and prev 
+    and prev + curr and see which one is actually bigger
+
+    */
+    public String largestNumber2(int[] nums) {
+        String[] arr = new String[nums.length];
+        int idx = 0;
+        for (int num : nums){
+            arr[idx++] = String.valueOf(num);
+        }
+
+        
+        Arrays.sort(arr, (a,b) ->{
+            String orda = a + b;
+            String ordb = b + a;
+
+            return ordb.compareTo(orda);
+        });
+
+         if (arr[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr){
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+
+     public String largestNumber(int[] nums) {
+        String[] arr = new String[nums.length];
+        int idx = 0;
+        for (int num : nums){
+            arr[idx++] = String.valueOf(num);
+        }
+
+        
+        Arrays.sort(arr, (a,b) ->{
+            String orda = a + b;
+            String ordb = b + a;
+
+            return ordb.compareTo(orda);
+        });
+
+         if (arr[0].equals("0")) {
+            return "0";
+        }
+
+       return String.join("", arr);
+    }
+
+}
